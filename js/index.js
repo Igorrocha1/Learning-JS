5d4f6e34e7863be6769d2a39137882caeca0d4b8
igor <irlima@magnasistemas.com.br>
2023-03-30 14:24:32 -0300

    aula 04

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-let a,b,c;
-a=5;
-b=6
-c=7; 
+var a = 1;
+var b = 5;
+var c = a * b;
+console.log(c);
+// = é um operador de atribuição 
+
+var PESSOA = "Dimitri";
+var pessoa = "Teixeira";
+
+console.log(pessoa);
+//JS é caseSensitive
+
